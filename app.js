10b71985de923e38f326e8b88774f4dbd2ecf330
Emil <s8emsajk@stud.uni-saarland.de>
2024-10-02 15:45:58 +0200

    Test

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("fetch-data").addEventListener("click", function() {
-    fetch('https://your-backend.herokuapp.com/api/data')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("data-output").innerText = data.message;
-    })
-    .catch(error => console.error('Fehler:', error));
-});
