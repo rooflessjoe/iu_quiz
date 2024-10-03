8525320dc0bb94d5e6a063437ff158726b0679a9
Emil <s8emsajk@stud.uni-saarland.de>
2024-10-03 17:39:19 +0200

    Backend URL update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("fetch-data").addEventListener("click", function() {
-    fetch('https://your-backend.herokuapp.com/api/data')
+    fetch('https://iu-quiz-backend.onrender.com/api/data')
     .then(response => response.json())
     .then(data => {
         document.getElementById("data-output").innerText = data.message;
