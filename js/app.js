f2c932b1ab5b2b9ce51c3c315dc4deae1c191703
Emil <s8emsajk@stud.uni-saarland.de>
2024-10-23 10:39:50 +0200

    Komponenten Test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-import { Home, Contact } from './components.js';
-import { About } from './components/about.js'
+//import { Home, About, Contact } from './components/*.js';
 
 // Haupt-App
 const App = {
@@ -13,16 +12,27 @@ const App = {
             currentComponent: 'Home'  // Standardmäßig Home anzeigen
         };
     },
-    components: {
-        Home,
-        About,
-        Contact
-    },
+    components: {},
     methods: {
         setCurrentComponent(component) {
             this.currentComponent = component;
         }
     }
 };
+
+// Dynamische Imports der Komponenten
+const components = import.meta.glob('./components/*.js');
+
+// Vor dem Mounten der App die Komponenten registrieren
+app.mixin({
+  async beforeCreate() {
+    // Dynamische Registrierung der Komponenten
+    for (const path in components) {
+      const module = await components[path](); // Komponente importieren
+      const componentName = path.split('/').pop().replace('.js', ''); // Name extrahieren
+      this.$options.components[componentName] = module.default; // In der Komponenten-Option registrieren
+    }
+  }
+});
 
 Vue.createApp(App).mount('#app');
