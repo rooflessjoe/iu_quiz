4d7cbe5501b6d3ed805811ea43ce5d2b1b022bcc
Emil <s8emsajk@stud.uni-saarland.de>
2024-11-16 18:35:51 +0100

    Test URL Alias

--- js/urlAlias.js
+++ js/urlAlias.js
@@ -1,13 +1,8 @@
+// Mappings für URL-Aliase
+const aliasMap = {
+    '/home': 'Home-Seite',
+    '/login': 'Login-Seite',
+    '/contact': 'Kontakt-Seite'
+};
 
-        // Prüfe, ob die URL auf /home oder /login umgeleitet werden soll
-        const aliasMap = {
-            '/iu_quiz/home': '/iu_quiz/index.html',
-            '/iu_quiz/login': '/iu_quiz/pages/login.html'
-        };
-    
-        const target = aliasMap[window.location.pathname];
-        if (target) {
-            // Lade die Zielseite, aber ersetze die URL in der Adressleiste
-            history.pushState({}, '', window.location.pathname);
-            window.location.replace(target);
-        }
+window.history.pushState({ page: window.location.pathname }, '', aliasMap[path]);
