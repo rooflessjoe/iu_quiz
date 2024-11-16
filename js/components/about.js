bf0e78f13f2a5aec669571ce28241522d23a6f4b
Emil <s8emsajk@stud.uni-saarland.de>
2024-11-16 13:14:03 +0100

    Lade-Button Implementierung

--- js/components/about.js
+++ js/components/about.js
@@ -53,7 +53,6 @@ export const About = {
                             this.message = 'Daten erfolgreich geladen!';
                         })
                         .catch(error => {
-                            this.loading = false;
                             console.error('Fehler beim Laden der Daten:', error);
                             this.message = 'Fehler beim Laden der Daten.';
                         })
