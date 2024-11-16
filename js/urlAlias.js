ac6e136a4ab8fd9bbc7a964fd4e9203597ffa944
Emil <s8emsajk@stud.uni-saarland.de>
2024-11-16 18:39:17 +0100

    Letzte Version / ToDo QuizApp

--- js/urlAlias.js
+++
@@ -1,8 +0,0 @@
-// Mappings für URL-Aliase
-const aliasMap = {
-    '/home': 'Home-Seite',
-    '/login': 'Login-Seite',
-    '/contact': 'Kontakt-Seite'
-};
-
-window.history.pushState({ page: window.location.pathname }, '', aliasMap[path]);
