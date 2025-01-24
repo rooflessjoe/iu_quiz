4e2a7e9a94a6f1ee2968f4722ed033f8e9f1a8b7
NiklasG-127 <niklasgeldbach@gmail.com>
2025-01-24 12:45:53 +0100

    Provisorischer Button zum Multiplayer Quiz in contact.js

--- js/components/contact.js
+++ js/components/contact.js
@@ -2,6 +2,7 @@ export const Contact = {
     template: `
     <div class="container-fluid">
         <h1>Kontakt</h1><p>So kannst du uns erreichen.</p>
+        <a class="btn btn-success" href="pages/quiz.html">Multiplayer Quiz</a>
     </div>
     `
 };
