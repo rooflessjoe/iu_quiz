db6d735ccf567da92dde5bd42f555d22735a8e41
Emil <emil.sajkevic@iu-study.org>
2025-02-27 14:09:38 +0100

    Test neue Views

--- js/components/home.js
+++ js/components/home.js
@@ -3,9 +3,7 @@ export const Home = {
     <div class="container-fluid">
         <h1>Home</h1>
         <p>Willkommen auf der Startseite!</p>
-        <div v-if="isLoggedIn>
-            <a class="btn btn-primary" role="button" href="pages/quiz.html">Quiz</a>
-        </div>
+        <a class="btn btn-primary" role="button" href="pages/quiz.html">Quiz</a>
     </div>
     `
 };
