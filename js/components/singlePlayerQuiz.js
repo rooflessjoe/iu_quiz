489b4e6513b6bdcd7f61fad36154eb96ae1b453e
Emil <emil.sajkevic@iu-study.org>
2025-03-05 12:57:09 +0100

    Design Test für Quiz Antwort

--- js/components/singlePlayerQuiz.js
+++ js/components/singlePlayerQuiz.js
@@ -8,8 +8,11 @@ export const singlePlayerQuiz = {
             <div v-for="(question, index) in quizData.questions" :key="index" class="mb-4">
                 <h5>Frage {{ index + 1 }}: {{ question.question }}</h5>
                 <ul class="list-group">
-                    <div :class="{'bg-success': valid, 'bg-danger': valid===false}">
                         <li v-for="(answer, ansIndex) in getAnswersForQuestion(question.question_id)" :key="ansIndex" class="list-group-item">
+                        <div :class="{
+                        'bg-success': valid, 
+                        'bg-danger': valid===false
+                        }">
                             <button class="btn btn-primary" @click.prevent="fetchDataAnswer(answer.question_id, answer.answer_id)">{{ answer.answer }}</button>
                         </li>
                     </div>
