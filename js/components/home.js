5553cc59e81ea7ee8dab413f400fee741a12b5e4
Emil <emil.sajkevic@iu-study.org>
2025-03-06 12:41:51 +0100

    Button für Multiplayer

--- js/components/home.js
+++ js/components/home.js
@@ -4,6 +4,7 @@ export const Home = {
         <h1>Home</h1>
         <p>Willkommen auf der Startseite!</p>
         <a class="btn btn-primary" role="button" href="pages/quiz.html">Einzelspieler</a>
+        <a class="btn btn-primary" role="button" href="pages/quizMulti.html">Mehrspieler</a>
     </div>
     `
 };
