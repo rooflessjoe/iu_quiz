ab4835e1dfdf8d34f93c01117b4559f618df40b8
Emil <emil.sajkevic@iu-study.org>
2025-03-13 13:27:05 +0100

    Barrierefreiheit optimiert

--- js/components/singlePlayerQuiz.js
+++ js/components/singlePlayerQuiz.js
@@ -6,13 +6,14 @@ export const singlePlayerQuiz = {
         <div v-if="quizData.questions && quizData.questions.length">
             <div v-for="(question, index) in quizData.questions" :key="index" class="mb-4">
                 <h5>Frage {{ index + 1 }}: {{ question.question }}</h5>
-                <ul class="list-group" role="radiogroup">
+                <ul class="list-group">
                         <li role="radio" v-for="(answer, ansIndex) in getAnswersForQuestion(question.question_id)" :key="ansIndex"
                         :class="['list-group-item', {'bg-success': this.selectedAnswers[question.question_id]?.answerID === answer.answer_id && this.selectedAnswers[question.question_id]?.valid,
                         'bg-danger': this.selectedAnswers[question.question_id]?.answerID === answer.answer_id && !this.selectedAnswers[question.question_id]?.valid}]"
                         @click.prevent="fetchDataAnswer(answer.question_id, answer.answer_id)" tabindex="0"
-                        :aria-checked="this.selectedAnswers[question.question_id]?.answerID === answer.answer_id">
-                            <span class="visually-hidden">Frage {{ index + 1 }}: {{ question.question }}</span>  
+                        :aria-checked="this.selectedAnswers[question.question_id]?.answerID === answer.answer_id"
+                        :aria-label="'Du hast die richtige Antwort ausgewählt': this.selectedAnswers[question.question_id]?.valid">
+                            <span class="visually-hidden" v-if="ansIndex===0">Frage {{ index + 1 }}: {{ question.question }}</span>  
                             <span class="visually-hidden">Antwort {{ ansIndex + 1 }}: </span>  
                             <span>{{ answer.answer }}</span>
                         </li>
